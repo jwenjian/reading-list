0ff098dbf2c259fc0267b2a1779f8e5d45af5c99
Jiang Wenjian <wenjian.jiang@foxmail.com>
2021-03-13 10:24:42 +0800

    add slogon first version of web share

--- template/page.js
+++ template/page.js
@@ -1,6 +1,11 @@
+
+// 设置当前时间
 const currentDate = new Date()
 document.getElementById('current-date').innerText = `${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-${currentDate.getDate()}`
 
+/**
+ * 获取数据
+ */
 fetch('./data.json')
 .then(async function (response) {
   const res = await response.json();
@@ -45,3 +50,25 @@ fetch('./data.json')
 
   recentList.appendChild(recentListFragment);
 })
+
+
+/**
+ * 处理分享按钮点击
+ * 
+ * @param {点击事件} evt 
+ */
+function handleShare(evt) {
+  console.log(navigator.share)
+  evt.preventDefault();
+  if (!navigator.share) {
+    console.warn('当前浏览器不支持 Web Share！');
+    return;
+  }
+
+  navigator.share({
+    text: document.querySelector('meta[name=description]').getAttribute('content'),
+    title: document.head.title,
+    url: window.location.href
+  }).then(() => {})
+  .catch(err => {console.error(err)})
+}
