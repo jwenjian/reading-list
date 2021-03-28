51eaf4466de20b1c197469fa97399c2fd0cdd2cc
jwenjian <jwenjian.me@gmail.com>
2021-03-28 10:47:40 +0000

    Deploying to gh-pages from  @ 8e550550ebe3bf79276909c73242a4021b2b7134 🚀

--- page.js
+++ page.js
@@ -16,8 +16,8 @@ setTimeout(() => {
 function handleShare(evt) {
   if (navigator.share) {
     navigator.share({
-      title: evt.innerText ? evt.innerText : 'TIR.cool 社区驱动的高质量阅读列表',
-      text: evt.innerText  ? evt.innerText : 'TIR.cool 社区驱动的高质量阅读列表',
+      title: evt.target.innerText ? evt.target.innerText : 'TIR.cool 社区驱动的高质量阅读列表',
+      text: evt.target.innerText  ? evt.target.innerText : 'TIR.cool 社区驱动的高质量阅读列表',
       url: 'https://tir.cool',
     })
       .then(() => console.log('Successful share'))
