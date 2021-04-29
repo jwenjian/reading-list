0467b8490fa66d1dfc7c4c7f88bcd4af897ca5b0
JIANG Wenjian <wenjian.jiang@thalesgroup.com>
2021-04-29 11:36:22 +0800

    fix api issue

--- index.js
+++ index.js
@@ -2,39 +2,12 @@ const { Octokit } = require("@octokit/rest")
 const fs = require('fs')
 const now = new Date()
 const startOfTheDay = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 0, 0, 0)
-const ejs = require('ejs')
 
 const octokit = new Octokit(
   {
-    auth: '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'
+    auth: process.env.GITHUB_TOKEN
   }
 );
-
-async function generateDetailPage() {
-  // detail page generation of issues in today
-  let issues = JSON.parse(fs.readFileSync('template/data.json')).today
-  issues.forEach(i => {
-    ejs.renderFile(
-      `template/detail.ejs`,
-      i,
-      (err, str) => {
-        fs.writeFileSync(`template/detail/${i.number}.html`, str)
-      }
-    )
-  });
-
-  // detail page generation of issues in recent
-  issues = JSON.parse(fs.readFileSync('template/data.json')).recent
-  issues.forEach(i => {
-    ejs.renderFile(
-      `template/detail.ejs`,
-      i,
-      (err, str) => {
-        fs.writeFileSync(`template/detail/${i.number}.html`, str)
-      }
-    )
-  });
-}
 
 async function main() {
   let data = {
@@ -53,14 +26,18 @@ async function main() {
     console.error('Cannot get response from GitHub')
     return
   }
-
+ 
   data.today = resp.data.filter(item => {
     return Date.parse(item.created_at) > startOfTheDay.getTime()
   })
 
   let rest = resp.data.slice(resp.data.indexOf(data.today[data.today.length - 1]) + 1)
 
-  data.recent = rest
+  if (rest.length > 20) {
+    data.recent = rest.slice(0, 20)
+  } else {
+    data.recent = rest
+  }
 
   const issues = resp.data
   console.log(`${issues.length} issues got from GitHub`)
@@ -68,13 +45,10 @@ async function main() {
   fs.writeFileSync('template/data.json', JSON.stringify(data))
   console.log('Issues write to data.json')
 
-  await generateDetailPage()
-
-  console.log("Detail page generated!")
 }
 
 main()
-  .catch(err => {
-    console.log(err)
-    process.exit(-1)
-  })
+.catch(err => {
+  console.log(err)
+  process.exit(-1)
+})
