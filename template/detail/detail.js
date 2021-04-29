dbda86b847884b283b4a4b94d43824ec7820ead4
JIANG Wenjian <wenjian.jiang@thalesgroup.com>
2021-04-29 11:47:06 +0800

    fix timeagao

---
+++ template/detail/detail.js
@@ -0,0 +1,26 @@
+
+function parseTime() {
+  const nodes = document.querySelectorAll('.timeago');
+
+  // use render method to render nodes in real time
+  timeago.render(nodes, 'zh_CN');
+  
+  // cancel all real-time render task
+  timeago.cancel();
+}
+
+setTimeout(() => {
+  parseTime()
+}, 0);
+
+function handleShare(evt) {
+  if (navigator.share) {
+    navigator.share({
+      title: evt.target.innerText ? evt.target.innerText : 'TIR.cool 社区驱动的高质量阅读列表',
+      text: evt.target.innerText  ? evt.target.innerText : 'TIR.cool 社区驱动的高质量阅读列表',
+      url: 'https://tir.cool',
+    })
+      .then(() => console.log('Successful share'))
+      .catch((error) => console.log('Error sharing', error));
+  }
+}
